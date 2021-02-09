82aeae4dc4214a949a149b15b28a3b394848a300
jampa-rambabu <76957828+jampa-rambabu@users.noreply.github.com>
2021-02-09 16:08:42 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -17,24 +17,26 @@ resource "aws_key_pair" "aws_tom" {
   public_key = file(var.key_p)
 }
 output "instance_ip_addr" {
-  value = format("Access the AWS hosted app from here: %s%s",aws_instance.aws_tom.*.public_ip, ":8080/PersistentWebApp")
+  value = format("Access the AWS hosted app from here: %s%s",aws_instance.aws_tom.public_dns, ":8080/PersistentWebApp")
 }
   resource "aws_instance" "aws_tom" {
-    ami = var.ami
+    key_name      = aws_key_pair.aws_tom.key_name
+    ami           = var.ami
     instance_type = var.ins_type
-    key_name = aws_key_pair.aws_tom.key_name
+    
     tags = {
       Name = "aws_tom"
     }
+    
     connection {
-      type = "ssh"
-      user = "ec2-user"
+      type        = "ssh"
+      user        = "ec2-user"
       private_key = file(var.key_p2)
-      host = self.public_ip
+      host        = self.public_ip
     }
 
   user_data = <<-EOF
- #!bin/bash
+  #!bin/bash
   sudo amazon-linux-extras install tomcat8.5
   sudo systemctl enable tomcat
   sudo systemctl start tomcat
@@ -69,7 +71,6 @@ variable "ins_type" {
 variable "key_p" {
   type = string
 }
-
 
 variable "key_p2" {
   type = string
